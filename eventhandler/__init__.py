c9777b402447076266714f63c4fbaa8f2c8f44db
rndmzd <rndmzd@protonmail.com>
2024-06-29 10:07:51 -0500

    Add class for execution of actions.

--- eventhandler/__init__.py
+++ eventhandler/__init__.py
@@ -1,1 +1,3 @@
 from eventhandler.eventhandler import EventHandler
+from eventhandler.actions import Actions
+from eventhandler.cbevents import CBEvents
---
+++ eventhandler/actions.py
@@ -0,0 +1,12 @@
+import logging
+
+logger = logging.getLogger('eventhandler.actions')
+logger.setLevel(logging.DEBUG)
+
+
+class Actions:
+    def __init__(self):
+        pass
+
+    def get_song_count(self, song_cost):
+        pass

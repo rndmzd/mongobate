034abe731d370c1a19f46461d955c7c5c8bb5e63
rndmzd <rndmzd@protonmail.com>
2024-06-29 10:09:34 -0500

    Add script to run standalone application without db.

---
+++ app-nodb.py
@@ -0,0 +1,62 @@
+import configparser
+import logging
+from logging.handlers import RotatingFileHandler
+import os
+import sys
+import time
+
+from eventhandler import EventHandler
+
+sys.stdout.reconfigure(encoding='utf-8')
+
+config = configparser.ConfigParser()
+config.read("config.ini")
+
+log_file = config.get("Logging", "log_file")
+log_max_size_mb = config.getint("Logging", "log_max_size_mb")
+log_backup_count = config.getint("Logging", "log_backup_count")
+
+if not os.path.exists(os.path.dirname(log_file)):
+    os.makedirs(os.path.dirname(log_file))
+
+logger = logging.getLogger('mongobate')
+logger.setLevel(logging.DEBUG)
+
+stream_handler = logging.StreamHandler()
+file_handler = RotatingFileHandler(
+    log_file,
+    maxBytes=log_max_size_mb * 1024 * 1024,
+    backupCount=log_backup_count,
+    encoding='utf-8'
+)
+
+formatter = logging.Formatter(
+    "%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+stream_handler.setFormatter(formatter)
+file_handler.setFormatter(formatter)
+
+logger.addHandler(stream_handler)
+logger.addHandler(file_handler)
+
+
+if __name__ == '__main__':
+    mongo_host = config.get("MongoDB", "host")
+    mongo_port = config.getint("MongoDB", "port")
+    mongo_db = config.get("MongoDB", "db")
+    mongo_collection = config.get("MongoDB", "collection")
+    
+    logger.debug('Initializing event handler.')
+    event_handler = EventHandler(
+        mongo_host, mongo_port, mongo_db, mongo_collection)
+
+    logger.debug('Running event handler.')
+    event_handler.run()
+
+    try:
+        while True:
+            time.sleep(0.1)
+    except KeyboardInterrupt:
+        logger.info("Shutting down...")
+        event_handler.stop()
+    finally:
+        logger.info("Application has shut down.")

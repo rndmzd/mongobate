c915a13dae6fd1a4878a143d32b4f004bcde4765
rndmzd <rndmzd@protonmail.com>
2024-06-29 01:48:44 -0500

    Logging optimization.

--- app.py
+++ app.py
@@ -39,8 +39,6 @@ formatter = logging.Formatter(
 stream_handler.setFormatter(formatter)
 file_handler.setFormatter(formatter)
 
-stream_handler.setLevel(logging.DEBUG)
-
 logger.addHandler(stream_handler)
 logger.addHandler(file_handler)
 

5dc4b833de8a7b86e55e905e32f3c683b610d60f
Ryan <141914774+rndmzd@users.noreply.github.com>
2024-07-23 00:28:19 -0500

    centralize config import to __init__.py

--- chataudio/__init__.py
+++ chataudio/__init__.py
@@ -1,1 +1,10 @@
 from chataudio.audioplayer import AudioPlayer
+
+"""import configparser
+import os
+from pathlib import Path
+
+config_path = Path(__file__).parent.parent / 'config.ini'
+
+config = configparser.ConfigParser()
+config.read(config_path)""""""

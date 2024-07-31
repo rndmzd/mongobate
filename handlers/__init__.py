c9d87198efe23fc35fbe43e520a37b96d04e5dba
rndmzd <rndmzd@protonmail.com>
2024-07-30 19:04:17 -0500

    Centralize config for handlers.

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,2 +1,11 @@
 from handlers.dbhandler import DBHandler
 from handlers.eventhandler import EventHandler
+
+import configparser
+import os
+from pathlib import Path
+
+config_path = Path(__file__).parent.parent / 'config.ini'
+
+config = configparser.ConfigParser()
+config.read(config_path)

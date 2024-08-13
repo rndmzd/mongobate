5ef38dfe93d16d56900cfac089e3b2e291f7fefa
rndmzd <rndmzd@protonmail.com>
2024-08-13 02:25:04 -0500

    Fix imports.

--- chatdj/__init__.py
+++ chatdj/__init__.py
@@ -1,6 +1,6 @@
-from chatdj.autodj import AutoDJ
-from chatdj.songextractor import SongExtractor
-#from chatdj.chatdj import AutoDJ, SongExtractor
+#from chatdj.autodj import AutoDJ
+#from chatdj.songextractor import SongExtractor
+from chatdj.chatdj import AutoDJ, SongExtractor
 
 import configparser
 import os
